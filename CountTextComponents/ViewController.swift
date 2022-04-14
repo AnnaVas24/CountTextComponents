092cc2b88eb2a82fc0fda81873b6104b38f24220
Anna <anna.vasi4ko@icloud.com>
2022-04-14 21:06:31 +0300

    Initial Commit

---
+++ CountTextComponents/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountTextComponents
+//
+//  Created by Vasichko Anna on 14.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
